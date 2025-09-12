e2fc296508ee7bab67683e0c503a6adfe8daed90
Frederic Luchting <frederic@luchting.de>
2025-09-12 19:53:36 +0000

    bump to dotnet 9.0

--- Threading/Startup.cs
+++ Threading/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Threading
 {
@@ -12,7 +13,7 @@ namespace Threading
             services.AddMvc(opt => opt.EnableEndpointRouting = false);
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
